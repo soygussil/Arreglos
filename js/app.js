c9f8a5d74affd70e7d18a39d387963a6a623bf29
Gustavo S <89321082+soygussil@users.noreply.github.com>
2022-01-11 16:13:45 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const modalImagen = document.querySelector('#modal-imagen');
+
+if(modalImagen){
+
+
+
+modalImagen.addEventListener('show.bs.modal', function (event) {
+    const enlace =event.relatedTarget;
+    const rutaImagen= enlace.getAttribute('data-bs-imagen');
+    
+    // construir imagen
+    const imagen=document.createElement('IMG');
+    imagen.src = `img/${rutaImagen}.jpg`;
+    imagen.classList.add('img-fluid');
+    imagen.alt= 'imagen-galeria';
+
+    const contenidoModal=document.querySelector('.modal-body');
+    contenidoModal.appendChild(imagen);
+    console.log(imagen);
+});
+
+modalImagen.addEventListener('hidden.bs.modal', function (event) {
+    const contenidoModal=document.querySelector('.modal-body');
+    contenidoModal.textContent=''; //limpiar contenido
+});
+
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
